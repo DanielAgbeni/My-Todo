93b748563a3ab7a2d2ae9ac75cc0e366cfb34ec4
DanielAgbeni <danielagbeni12@gmail.com>
2023-08-20 12:30:15 +0100

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Footer = () => {
 	return (
 		<footer>
-			<p>Copyright &copy; {new Date().getFullYear()}</p>
+			<p>Copyright &copy; Daniel Agbeni {new Date().getFullYear()}</p>
 			<Link to='/about'>About</Link>
 		</footer>
 	)
